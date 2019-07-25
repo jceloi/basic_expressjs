ea692dfeef2691737311e43ca0c91bb5bc1a26ac
Eloi Jean-Charles <jeloi@oui.sncf>
2019-07-25 11:34:38 +0200

    Ajout middlewares

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+
 app.post('/', function (req, res) {
-    res.send('OK')
     console.log(`Request : ${JSON.stringify(req.params)} - ${JSON.stringify(req.query)} - ${JSON.stringify(req.body)} `);
+    res.send('OK')
+
 })
 
 app.listen(8080, function () {
