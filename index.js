e451cd048a2867cbac605b653ac6f27678d48ea7
Eloi Jean-Charles <jeloi@oui.sncf>
2019-07-25 11:31:11 +0200

    jout de logs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.post('/', function (req, res) {
     res.send('OK')
-    console.log(`Request : ${JSON.stringify(req)}`);
+    console.log(`Request : ${JSON.stringify(req.params)} - ${JSON.stringify(req.query)} - ${JSON.stringify(req.body)} `);
 })
 
 app.listen(8080, function () {
